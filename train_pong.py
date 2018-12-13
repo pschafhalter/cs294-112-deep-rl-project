6453e32a9d4c9ef4eaffc866b8fa3f570560d740
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-12-12 16:10:17 -0800

    Update train_pong.py

--- train_pong.py
+++ train_pong.py
@@ -4,25 +4,58 @@ from ray.tune import run_experiments, grid_search
 from ray.rllib.models import ModelCatalog, Model
 from ray.rllib.models.misc import flatten
 import tensorflow as tf
-from tensorflow import layers
+import tensorflow.contrib.slim as slim
 
 
 class LowResModel(Model):
     def _build_layers(self, inputs, num_outputs, options):
-        with tf.name_scope("fc_net"):
-            last_layer = layers.conv2d(
+        dim = options["dim"]
+        if dim == 84:
+            filters = [
+                [16, [8, 8], 4],
+                [32, [4, 4], 2],
+                [256, [11, 11], 1],
+            ]
+        elif dim == 42:
+            filters = [
+                [16, [4, 4], 2],
+                [32, [4, 4], 2],
+                [256, [11, 11], 1],
+            ]
+        elif dim == 21:
+            filters = [
+                [16, [2, 2], 1],
+                [32, [4, 4], 2],
+                [256, [11, 11], 1],
+            ]
+
+        activation = tf.nn.relu
+
+        with tf.name_scope("vision_net"):
+            for i, (out_size, kernel, stride) in enumerate(filters[:-1], 1):
+                inputs = slim.conv2d(
                     inputs,
-                    16,
-                    (4, 4),
-                    activation=tf.nn.relu)
-            last_layer = layers.conv2d(
-                    inputs,
-                    32,
-                    (4, 4),
-                    activation=tf.nn.relu)
-            last_layer = flatten(last_layer)
-            output = tf.layers.dense(last_layer, num_outputs, activation=None)
-            return output, last_layer
+                    out_size,
+                    kernel,
+                    stride,
+                    activation_fn=activation,
+                    scope="conv{}".format(i))
+            out_size, kernel, stride = filters[-1]
+            fc1 = slim.conv2d(
+                inputs,
+                out_size,
+                kernel,
+                stride,
+                activation_fn=activation,
+                padding="VALID",
+                scope="fc1")
+            fc2 = slim.conv2d(
+                fc1,
+                num_outputs, [1, 1],
+                activation_fn=None,
+                normalizer_fn=None,
+                scope="fc2")
+            return flatten(fc2), flatten(fc1)
 
 ModelCatalog.register_custom_model("LowResModel", LowResModel)
 
@@ -30,20 +63,19 @@ ModelCatalog.register_custom_model("LowResModel", LowResModel)
 if __name__ == "__main__":
     ray.init()
     run_experiments({
-        "low-res-car-pong": {
+        "low-res-pong-v3": {
             "run": "A3C",
             "env": "PongNoFrameskip-v4",
-            "checkpoint_freq": 100,
+            # "checkpoint_freq": 100,
             "config": {
-                "num_gpus": 0.33,
-                "num_workers": 3,
+                "num_workers": 8,
                 "model": {
+                    "dim": grid_search([84, 42, 21]),
                     "custom_model": "LowResModel",
                 },
-                # "model": {"dim": grid_search([84, 42, 21, 10])},
             },
             "stop" : {
-                "episode_reward_mean": 1000
+                "episode_reward_mean": 20
             },
             }
         })
